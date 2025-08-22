8eda733ecdabf3139093d7a67e4bf484cac7ba3c
rifkif-trial <rifus.industries@gmail.com>
2025-08-22 21:37:27 +0700

    push trial

--- cypress/e2e/3-test/login.cy.js
+++ cypress/e2e/3-test/login.cy.js
@@ -1,10 +1,10 @@
-describe ('Scenario Login', () => {
-    it('TC001-Valid Login', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('#user-name').type('standard_user')
+describe ('Scenario Verifikasi Fungsi Login', () => {
+    it('TC001-Login dengan username valid & password valid', () => {
+        cy.visit('https://www.saucedemo.com/v1/index.html')
+        cy.get('#user-name').type('standard_user').should('have.value','standard_user')
         cy.get('#password').type('secret_sauce')
-        cy.get('.submit-button.btn_action').click()
-        cy.get('#login-button').should('be.visible')
+        cy.get('.btn_action').should('be.visible')
+        cy.get('.btn_action').click()
         cy.url().should('include','inventory')
     })
 })
