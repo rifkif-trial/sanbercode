b653a7c80b7dadec5f7e9d3940a06caaeba66d1d
rifkif-trial <rifus.industries@gmail.com>
2025-08-22 09:15:36 +0700

    update

--- cypress/e2e/3-test/login.cy.js
+++ cypress/e2e/3-test/login.cy.js
@@ -4,6 +4,7 @@ describe ('Scenario Login', () => {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('.submit-button.btn_action').click()
+        cy.get('#login-button').should('be.visible')
         cy.url().should('include','inventory')
     })
 })
